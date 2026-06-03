41fae4bc647a79ed6f8758f9a6800ceaf4e12b81
rudrapatel2411 <rudrarp2006@gmail.com>
2026-06-03 16:54:56 +0530

    fix: correct dotenv path resolution in reseed.js to target server/.env

--- server/utils/reseed.js
+++ server/utils/reseed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
 const Student = require('../models/Student');
 const seedStudents = require('./seeder');
 
